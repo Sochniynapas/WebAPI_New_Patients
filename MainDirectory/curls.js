5c4221bd08ba50aa2abba36da7f116571afa5368
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2023-12-23 01:57:01 +0700

    Started to making fucntions for profile

--- MainDirectory/curls.js
+++ MainDirectory/curls.js
@@ -2,3 +2,4 @@ export const getProfile = "https://mis-api.kreosoft.space/api/doctor/profile";
 export const getSpecialities = "https://mis-api.kreosoft.space/api/dictionary/speciality?page=1&size=5";
 export const registration = "https://mis-api.kreosoft.space/api/doctor/register";
 export const login = "https://mis-api.kreosoft.space/api/doctor/login";
+export const profile = "https://mis-api.kreosoft.space/api/doctor/profile";
