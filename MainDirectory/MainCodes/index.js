f3ba5da32803eb1bcec9753ada66f87154927ca3
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2023-12-23 03:19:08 +0700

    Made a filter card, but without needed filters

--- MainDirectory/MainCodes/index.js
+++ MainDirectory/MainCodes/index.js
@@ -7,6 +7,7 @@ const logoutItem = document.getElementById('logoutItem');
 const pathName = window.location.pathname;
 // export const regularForConcretePost = /^\/post\/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$/;
 // export const regularForCommunity = /^\/communities\/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}).*$/;
+export const regularForPatients = /^\/patients(?:\?name=[a-zA-Z]+)?(?:&conclusions=(?:Disease|Recovery|Death(?:,Disease|,Recovery|,Death)*)+)?(?:&sorting=(?:NameAsc|NameDesc|CreateAsc|CreateDesc|InspectionAsc|InspectionDesc)?)?(?:&scheduledVisits=(?:true|false)?)?(?:&onlyMine=(?:true|false)?)?(?:&page=\d+)?(?:&size=\d+)?$/;
 
 
 switch (pathName){
@@ -78,42 +79,58 @@ switch (pathName){
         break;
     }
 
+    default:
+        // if (regularForConcretePost.test(pathName)) {
+        //     response = await fetch('/postDirectory/concretePost.html');
+        //     dataForResponse = await response.text();
+        //     contentOfACard = document.getElementById('concreteCard');
+        //     contentOfACard.innerHTML = dataForResponse;
+        //     await checkUserLogging();
+        //     contentOfACard.querySelectorAll('script').forEach(script => {
+        //         const newScript = document.createElement("script")
+        //         Array.from(script.attributes).forEach(attr => {
+        //             newScript.setAttribute(attr.name, attr.value)
+        //         })
+        //         newScript.appendChild(document.createTextNode(script.innerHTML))
+        //         script.parentNode.replaceChild(newScript, script)
+        //     });
+        // }
+        // else if(regularForCommunity.test(pathName)){
+        //     response = await fetch('/mainPageOfCommunitiesDirectory/mainPageOfCommunities.html');
+        //     dataForResponse = await response.text();
+        //     contentOfACard = document.getElementById('concreteCard');
+        //     contentOfACard.innerHTML = dataForResponse;
+        //     await checkUserLogging();
+        //     contentOfACard.querySelectorAll('script').forEach(script => {
+        //         const newScript = document.createElement("script")
+        //         Array.from(script.attributes).forEach(attr => {
+        //             newScript.setAttribute(attr.name, attr.value)
+        //         })
+        //         newScript.appendChild(document.createTextNode(script.innerHTML))
+        //         script.parentNode.replaceChild(newScript, script)
+        //     });
+        // }
+        if(regularForPatients.test(pathName)){
+            response = await fetch('PatientsDirectory/patientsCard.html');
+            dataForResponse = await response.text();
+            contentOfACard = document.getElementById('concreteCard');
+            contentOfACard.innerHTML = dataForResponse;
+            await checkUserToken();
 
-    // default:
-    //     if (regularForConcretePost.test(pathName)) {
-    //         response = await fetch('/postDirectory/concretePost.html');
-    //         dataForResponse = await response.text();
-    //         contentOfACard = document.getElementById('concreteCard');
-    //         contentOfACard.innerHTML = dataForResponse;
-    //         await checkUserLogging();
-    //         contentOfACard.querySelectorAll('script').forEach(script => {
-    //             const newScript = document.createElement("script")
-    //             Array.from(script.attributes).forEach(attr => {
-    //                 newScript.setAttribute(attr.name, attr.value)
-    //             })
-    //             newScript.appendChild(document.createTextNode(script.innerHTML))
-    //             script.parentNode.replaceChild(newScript, script)
-    //         });
-    //     }
-    //     else if(regularForCommunity.test(pathName)){
-    //         response = await fetch('/mainPageOfCommunitiesDirectory/mainPageOfCommunities.html');
-    //         dataForResponse = await response.text();
-    //         contentOfACard = document.getElementById('concreteCard');
-    //         contentOfACard.innerHTML = dataForResponse;
-    //         await checkUserLogging();
-    //         contentOfACard.querySelectorAll('script').forEach(script => {
-    //             const newScript = document.createElement("script")
-    //             Array.from(script.attributes).forEach(attr => {
-    //                 newScript.setAttribute(attr.name, attr.value)
-    //             })
-    //             newScript.appendChild(document.createTextNode(script.innerHTML))
-    //             script.parentNode.replaceChild(newScript, script)
-    //         });
-    //     }
-    //     else {
-    //         window.location.href = 'https://ru.hostings.info/upload/images/2021/12/e11044b915dc39afc3004430606bd6d1.jpg';
-    //     }
-    //     break;
+            contentOfACard.querySelectorAll('script').forEach(script => {
+                const newScript = document.createElement("script")
+                Array.from(script.attributes).forEach(attr => {
+                    newScript.setAttribute(attr.name, attr.value)
+                })
+                newScript.appendChild(document.createTextNode(script.innerHTML))
+                script.parentNode.replaceChild(newScript, script)
+            });
+            break;
+        }
+        else {
+            window.location.href = 'https://ru.hostings.info/upload/images/2021/12/e11044b915dc39afc3004430606bd6d1.jpg';
+        }
+        break;
 }
 debugger
 logoutItem.addEventListener('click', removeTokenFromLocalStorage);
