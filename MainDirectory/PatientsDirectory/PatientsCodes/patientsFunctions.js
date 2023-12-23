11f2ea69322c8f51d366e00cca83273db54fc816
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2023-12-23 18:06:09 +0700

    Started to making a lodaing of patients view logic

--- MainDirectory/PatientsDirectory/PatientsCodes/patientsFunctions.js
+++ MainDirectory/PatientsDirectory/PatientsCodes/patientsFunctions.js
@@ -1,5 +1,109 @@
 import {getPatients} from "../../curls.js";
 
+
+export async function createPost(data) {
+    const token = localStorage.getItem('token');
+    const postContainer = document.getElementById('posts-container');
+    const likeIcon = data.hasLike ? '💜' : '🖤';
+    const community = data.communityName ? ` в сообществе "${data.communityName}"` : "";
+    const response = await fetch('../postView/postView.html');
+    const postString = await response.text();
+    const postHTML = document.createElement('div');
+    const addressText = await getChain(data.addressId);
+    postHTML.innerHTML = postString;
+
+    postHTML.querySelector('#info').textContent = data.author + " - " + dateInNormalView(data.createTime)  + community;
+    postHTML.querySelector('.title-text').textContent = data.title;
+    postHTML.querySelector('#postText').textContent = data.description;
+    postHTML.querySelector('#tagForm').innerHTML = `${data.tags.map(tag => `<span class="badge badge-secondary text-secondary" id="tag">#${tag.name}</span>`).join('')}`;
+    postHTML.querySelector('.reading-time').textContent = "Время чтения: "+ data.readingTime + " мин.";
+    if(addressText!=='') {
+        postHTML.querySelector('.address').textContent = addressText;
+        postHTML.querySelector('.address').classList.add('d-none');
+    }
+    postHTML.querySelector('#commentsCount').textContent = data.commentsCount;
+    postHTML.querySelector('#likeCount').textContent = data.likes;
+    postHTML.querySelector('#likeIcon').textContent = likeIcon;
+
+    loadImage(data.image, (isValid) => {
+        if (isValid) {
+            postHTML.querySelector('#img').src = data.image;
+            postHTML.querySelector('#img').classList.add('border', 'border-secondary')
+        }
+    })
+    postContainer.appendChild(postHTML);
+    const postText = postHTML.querySelector('#postText')
+    const isTextOverflowing = postText.scrollHeight > postText.clientHeight;
+    const readMoreBtn = postHTML.querySelector('.read-more-btn');
+    const like = postHTML.querySelector('#likeIcon');
+    const likeCounter = postHTML.querySelector('#likeCount');
+
+    if (isTextOverflowing) {
+        readMoreBtn.style.display = "block";
+    }
+
+    postHTML.querySelector('.title-text').addEventListener('click', async function () {
+        console.log(localStorage.getItem('token'));
+        window.location.href = `/post/${data.id}`;
+
+    });
+    like.addEventListener('click', async function () {
+        if (like.textContent === '💜') {
+            try {
+                const response = await fetch(`https://blog.kreosoft.space/api/post/${data.id}/like`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+                if (response.ok) {
+                    like.textContent = '🖤';
+                    likeCounter.textContent = `${parseInt(likeCounter.textContent) - 1}`;
+                }
+            } catch (error) {
+                console.error('Ошибка запроса')
+            }
+
+        } else {
+            try {
+                const response = await fetch(`https://blog.kreosoft.space/api/post/${data.id}/like`, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
+                if (response.ok) {
+                    like.textContent = '💜';
+                    likeCounter.textContent = `${parseInt(likeCounter.textContent) + 1}`;
+                }
+            } catch (error) {
+                console.error('Ошибка запроса')
+            }
+        }
+    });
+    postContainer.addEventListener('click', async function (event) {
+        let readMoreBtn = event.target.closest('.read-more-btn');
+        if (readMoreBtn) {
+            let postText = readMoreBtn.previousElementSibling;
+            postText.classList.remove('clamp-6');
+            readMoreBtn.style.display = "none";
+        }
+    });
+}
+
+async function displayPatients(patients) {
+    console.log(patients);
+    for (const patient of patients) {
+        try {
+            await createPatient(patient);
+        } catch (error) {
+            console.error(`Ошибка загрузки картинки: ${error}`);
+        }
+    }
+}
+
 async function updatePageFromUrl() {
     const currentParams = new URLSearchParams(window.location.search);
 
@@ -46,18 +150,21 @@ async function updatePageFromUrl() {
 
     if (response.ok) {
         const data = await response.json();
-        const postContainer = document.getElementById('own-posts');
-        document.getElementById('posts-container').remove();
-        const forPostsFiltred = document.createElement('div');
-        forPostsFiltred.id = 'posts-container';
-        postContainer.prepend(forPostsFiltred);
+        const postContainer = document.getElementById('own-patients');
+        document.getElementById('patients-container').remove();
+        const forPostsFiltered = document.createElement('div');
+        forPostsFiltered.id = 'patients-container';
+        postContainer.prepend(forPostsFiltered);
 
-        await displayPosts(data.posts);
+        await displayPatients(data.patients);
         await displayPageControllers(data, page);
     } else {
         throw new Error("Ошибка в запросе");
     }
 }
+
+
+
 
 
 export async function initializePage(){
